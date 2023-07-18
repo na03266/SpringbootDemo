f1b9474ecf5ff4bcecb4a719879a30772e1f0d0e
HwangjeNa <na03266@naver.com>
2023-07-18 11:24:11 +0900

    JDK 버전 17로 다운 maven도 똑같이 다운 오타 수정

---
+++ src/main/java/me/hwangje/springbootdeveloper/SpringBootDeveloperApplocation.java
@@ -0,0 +1,13 @@
+package me.hwangje.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplocation {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootDeveloperApplocation.class, args);
+    }
+}
