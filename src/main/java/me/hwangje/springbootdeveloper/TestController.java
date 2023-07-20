409aaa05d6fe9b44b97d5cb996eb491703496c16
HwangjeNa <na03266@naver.com>
2023-07-20 16:32:40 +0900

    컨트롤러 수정, sql 구문 삽입, 어플리케이션 yml 수정

--- src/main/java/me/hwangje/springbootdeveloper/TestController.java
+++ src/main/java/me/hwangje/springbootdeveloper/TestController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Member;
 import java.util.List;
 
 @RestController
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into member (id, name) values (1, 'name1')
+insert into member (id, name) values (2, 'name2')
+insert into member (id, name) values (3, 'name3')
