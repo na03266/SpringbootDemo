edb45288784f3648cb69cb8b12d9bd43ae17d795
HwangjeNa <na03266@naver.com>
2023-07-21 14:41:42 +0900

    테스트용 어노테이션 테스트

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -13,14 +13,5 @@ public class JUnitTest {
         Assertions.assertEquals(sum, a+b); //값이 같은지 확인
 
     }
-    @DisplayName("1+2는 3이다") //테스트 이름
-    @Test //테스트 메서드
-    public void junitFailedTest(){
-        int a = 1;
-        int b = 3;
-        int sum = 3;
-
-        Assertions.assertEquals(sum, a+b); //값이 같은지 확인
 
     }
-}
