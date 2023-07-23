2946d92afc84e515f52fbc55714fb4a5a83a9e88
HwangjeNa <na03266@naver.com>
2023-07-23 13:39:48 +0900

    테스트 작업 삭제

--- src/main/java/me/hwangje/springbootdeveloper/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package me.hwangje.springbootdeveloper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into member (id, name) values (1, 'name1')
-insert into member (id, name) values (2, 'name2')
-insert into member (id, name) values (3, 'name3')
