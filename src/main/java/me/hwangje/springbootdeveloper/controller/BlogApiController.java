74decebf612bacb903ca17b9036a2a1623a5c062
HwangjeNa <na03266@naver.com>
2023-07-24 08:12:39 +0900

    글목록 조회 후 반환 controller 메소드 작성

--- src/main/java/me/hwangje/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/hwangje/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.hwangje.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.hwangje.springbootdeveloper.domain.Article;
 import me.hwangje.springbootdeveloper.dto.AddArticleRequest;
+import me.hwangje.springbootdeveloper.dto.ArticleResponse;
 import me.hwangje.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -25,5 +29,17 @@ public class BlogApiController {
         //요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ArticleResponse>> findAllArticle(){ // 글을 조회한 뒤 반환
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+
     }
 }
