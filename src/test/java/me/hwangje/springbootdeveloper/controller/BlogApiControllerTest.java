f00badf37bc41adb7cefe5692a9bdabcb135dd8b
HwangjeNa <na03266@naver.com>
2023-07-24 08:29:15 +0900

    블로그 글 목록 조회 테스트코드 작성

--- src/test/java/me/hwangje/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/hwangje/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -20,7 +20,9 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -40,36 +42,62 @@ class BlogApiControllerTest {
     BlogRepository blogRepository;
 
     @BeforeEach //테스트 실행 전 실행하는 메서드
-    public void setMockSetUp(){
+    public void setMockSetUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
         blogRepository.deleteAll();
     }
-@DisplayName("addArticle: 블로그 글 추가에 성공한다.")
+
+    @DisplayName("addArticle: 블로그 글 추가에 성공한다.")
     @Test
-    public void addArticle() throws Exception{
+    public void addArticle() throws Exception {
         //given 블로그 글 추가에 필요한 요청 객체 생성
-    final String url = "/api/articles";
-    final String title = "title";
-    final String content = "content";
-    final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
 
-    //객체 json으로 직렬화
-    final String requestBody = objectMapper.writeValueAsString(userRequest);
+        //객체 json으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
 
-    //when, 블로그 글 추가에 API요청, 제이슨 타입
-    //설정한 내용을 바탕으로 요청 전송
-    ResultActions result = mockMvc.perform(post(url)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(requestBody));
-    //then
-    result.andExpect(status().isCreated());
+        //when, 블로그 글 추가에 API요청, 제이슨 타입
+        //설정한 내용을 바탕으로 요청 전송
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+        //then
+        result.andExpect(status().isCreated());
 
-    List<Article> articles = blogRepository.findAll();
+        List<Article> articles = blogRepository.findAll();
 
-    assertThat(articles.size()).isEqualTo(1);    //크기가 1인지 검증
-    assertThat(articles.get(0).getTitle()).isEqualTo(title);
-    assertThat(articles.get(0).getContent()).isEqualTo(content);
+        assertThat(articles.size()).isEqualTo(1);    //크기가 1인지 검증
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
 
-}
+    }
+
+    @DisplayName("findAllArticles: 블로그 글 목록 조회에 성공한다.")
+    @Test
+    public void findAllArticles() throws Exception {
+        //given 블로그 글 저장
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        //when 목록조회 api 호출
+        final ResultActions resultActions = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then 응답 코드가 200 ok 이고 반환값중 0번째 요소의 컨텐츠와 타이틀에 저장된 값이 같은지 확인
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
+
+    }
 }
