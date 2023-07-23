08aae9512814ae603781f427108ea0cf7575bc13
HwangjeNa <na03266@naver.com>
2023-07-23 14:09:01 +0900

    블로그 서비스 구현

---
+++ src/main/java/me/hwangje/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,21 @@
+package me.hwangje.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hwangje.springbootdeveloper.domain.Article;
+import me.hwangje.springbootdeveloper.dto.AddArticleRequest;
+import me.hwangje.springbootdeveloper.repository.BlogRepository;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // final 이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service // 빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    private Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+
+
+}
