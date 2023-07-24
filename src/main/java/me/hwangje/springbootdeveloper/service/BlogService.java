df4158f3bb98d07fee98208d6404820fda7afdb7
HwangjeNa <na03266@naver.com>
2023-07-24 10:54:07 +0900

    데이터베이스에 저장된 글의 ID를 이용한 글 조회 메서드 제작(blogservice)

--- src/main/java/me/hwangje/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/hwangje/springbootdeveloper/service/BlogService.java
@@ -22,4 +22,9 @@ public class BlogService {
         return blogRepository.findAll();
     }
 
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found: " +id));
+    }
+
 }
