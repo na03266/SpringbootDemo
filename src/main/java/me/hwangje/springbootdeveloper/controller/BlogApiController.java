b151139441b08321e5cd2f40dd482aa5bfbbfc17
HwangjeNa <na03266@naver.com>
2023-07-24 11:12:09 +0900

    데이터베이스에 저장된 글의 ID를 이용한 글 조회 메서드 get 요청시  매핑 메서드 작성(BlogApiController)

--- src/main/java/me/hwangje/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/hwangje/springbootdeveloper/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import me.hwangje.springbootdeveloper.dto.ArticleResponse;
 import me.hwangje.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +37,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
 
+    @GetMapping("/api/articles/{id}")
+    //URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id){
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
