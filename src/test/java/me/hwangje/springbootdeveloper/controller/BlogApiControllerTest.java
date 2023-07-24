41a44a5d1746da3fdc478167a998cfea67403cea
HwangjeNa <na03266@naver.com>
2023-07-24 16:52:09 +0900

    assertthat import문 인터페이스 타입으로 변경후 오류 해결

--- src/test/java/me/hwangje/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/hwangje/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -18,9 +18,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -122,6 +121,29 @@ class BlogApiControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").value(content))
                 .andExpect(jsonPath("$.title").value(title));
-
         }
+
+    @DisplayName("deleteArticle : 블로그 글 삭제에 성공한다")
+    @Test
+    public void deleteArticle() throws Exception {
+
+        //given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        //when
+        mockMvc.perform(delete(url, savedArticle.getId()))
+                .andExpect(status().isOk());
+
+        //then
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles).isEmpty();
     }
+}
