5a092aebcb00ffdae753dacf87d43b39f68b6880
HwangjeNa <na03266@naver.com>
2023-07-25 09:08:50 +0900

    타임리프 문법 연습

---
+++ src/main/java/me/hwangje/springbootdeveloper/controller/ExampleController.java
@@ -0,0 +1,38 @@
+package me.hwangje.springbootdeveloper.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller //컨트롤러 라는 것을 명시
+public class ExampleController {
+
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model){ //뷰로 데이터를 넘겨주는 모델 객체
+        Person examplePerson = new Person();
+
+        examplePerson.setId(1L);
+        examplePerson.setName("홍길동");
+        examplePerson.setAge(11);
+        examplePerson.setHobbies(List.of("운동", "독서"));
+
+        model.addAttribute("person", examplePerson); //Person 객체 생성
+        model.addAttribute("today", LocalDate.now()); //
+
+        return "example"; // example.html 라는 뷰 조회
+    }
+
+    @Setter
+    @Getter
+    class Person {
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
