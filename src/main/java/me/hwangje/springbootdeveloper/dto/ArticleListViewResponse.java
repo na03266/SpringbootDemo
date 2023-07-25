9048ddeb9619d723cf00226b68d83015b94ba835
HwangjeNa <na03266@naver.com>
2023-07-26 08:47:48 +0900

    글목록 뷰 구현, 뷰에게 데이터 전달을 위한 객체 생성

---
+++ src/main/java/me/hwangje/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,19 @@
+package me.hwangje.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.hwangje.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
+
+}
