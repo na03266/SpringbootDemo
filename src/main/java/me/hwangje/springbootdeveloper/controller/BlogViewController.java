b1253e24ce16f8c66de4b259e5447cf9cf5da4f6
HwangjeNa <na03266@naver.com>
2023-07-28 17:27:55 +0900

    크리에이트 시간을 뷰에서 사용할 DTO에 해당하는 컨트롤러 겟 아티클 메서드 추가

--- src/main/java/me/hwangje/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/hwangje/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.hwangje.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.hwangje.springbootdeveloper.domain.Article;
 import me.hwangje.springbootdeveloper.dto.ArticleListViewResponse;
+import me.hwangje.springbootdeveloper.dto.ArticleViewResponse;
 import me.hwangje.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); //블로그 글 리스트 저장
 
         return "articleList"; //articleList.html라는 뷰 를 찾을 수 있도록 뷰의 이름 적음
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
