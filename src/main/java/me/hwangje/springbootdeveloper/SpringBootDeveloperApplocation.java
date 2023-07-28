9e2f3f92d6d1cc0e54da0b8f972e0ced39ff2891
HwangjeNa <na03266@naver.com>
2023-07-28 17:20:18 +0900

    엔티티의 created_at, updated_at을 자동으로 수정하도록 애너테이션 추가

--- src/main/java/me/hwangje/springbootdeveloper/SpringBootDeveloperApplocation.java
+++ src/main/java/me/hwangje/springbootdeveloper/SpringBootDeveloperApplocation.java
@@ -2,9 +2,11 @@ package me.hwangje.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.swing.*;
 
+@EnableJpaAuditing //created_at, updated_at 자동 업데이트
 @SpringBootApplication
 public class SpringBootDeveloperApplocation {
     public static void main(String[] args){
