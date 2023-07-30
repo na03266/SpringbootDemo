989027a9aac994f3f22bc39089c9301e2eafd3dc
HwangjeNa <na03266@naver.com>
2023-07-30 10:11:14 +0900

    수정 / 생성 기능을 위한 API 구현

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,8 +1,8 @@
 //삭제 기능
 const deleteButton = document.getElementById('delete-btn');
 
-if(deleteButton){
-    deleteButton.addEventListener('click',event =>{ //클릭 이벤트 발생시
+if (deleteButton) {
+    deleteButton.addEventListener('click', event => { //클릭 이벤트 발생시
         let id = document.getElementById('article-id').value;
         fetch(`/api/articles/${id}`, { //패치 메서드로 딜리트 요청
             method: 'DELETE'
@@ -11,5 +11,32 @@ if(deleteButton){
                 alert('삭제가 완료되었습니다.'); //요청 완료 메세지
                 location.replace('/articles'); //실행시 사용자의 화면을 시작 화면으로 이동.
             });
+    });
+}
+
+//수정 기능
+// id가 modify-btn 인 엘리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: `PUT`,
+            headers: {
+                "content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+        .then(() => {
+            alert('수정이 완료되었습니다.');
+            location.replace(`/articles/${id}`);
         });
+    });
+
 }
