16aaa590ca31f9ecb2831c0b85e764234a773c59
HwangjeNa <na03266@naver.com>
2023-07-30 09:41:10 +0900

    수정화면 쇼를 위한 컨트롤러 메서드 추가

--- src/main/java/me/hwangje/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/hwangje/springbootdeveloper/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,5 +35,17 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("new-article")
+    //id 키를 가진 쿼리 파라미터의 값을 id 변수에 메핑(id 없을수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) { //id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else { //id가 없으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
