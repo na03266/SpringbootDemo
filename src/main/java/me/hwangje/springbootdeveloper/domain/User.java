0959f61908bbb3e09fef5c9601e6516bfc3b84a6
HwangjeNa <na03266@naver.com>
2023-08-01 08:41:38 +0900

    유저 정보 클래스 생성 완료 의존성 오류 진행중

--- src/main/java/me/hwangje/springbootdeveloper/domain/User.java
+++ src/main/java/me/hwangje/springbootdeveloper/domain/User.java
@@ -36,4 +36,42 @@ public class User implements UserDetails { //UserDetails를 상속받아 인증 
     public Collection<? extends GrantedAuthority> getAuthorities(){
         return List.of(new SimpleGrantedAuthority("user"));
     }
+
+    //사용자의 id 를 반환(고유한 값)
+    @Override
+    public String getUsertname(){
+        return email;
+    }
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    //계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired(){
+    //만료되었는지 확인하는 로직
+        return true; //true -> 만료되지 않았음
+    }
+
+    //계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNoLock(){
+        //잠금되었는지 확인하는 로직
+        return true; //true -> 잠금되지 않았음
+    }
+
+    //패스워드 만료 여부 반환
+    @Override
+    public boolean isAccountNoLock(){
+        //패스워드가 만료 되었는지 확인하는 로직
+        return true; //true -> 잠금되지 않았음
+    }
+
+    //계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled(){
+        // 계정이 사용 가능한지 확인하는 로직
+        return true // true -> 사용 가능
+    }
 }
