38ca9323ecb0ede10d718a24443b3771b3a01906
HwangjeNa <na03266@naver.com>
2023-07-31 09:13:56 +0900

    생성기능 작성

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -40,3 +40,26 @@ if (modifyButton) {
     });
 
 }
+
+//등록 기능
+// id가 create-btn인 엘리먼트
+const createButton = document.getElementById("cteate-btn")
+
+if(createButton){
+    //클릭 이벤트가 감지되면 생성 API 요청
+    createButton.addEventListener("click", (event) => {
+        fetch("/api/articles",{
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById("title").value,
+                content: document.getElementById("content").value,
+            }),
+        }).then(() =>{
+            alert("등록 완료되었습니다.");
+            location.replace("/articles");
+        });
+    });
+}
