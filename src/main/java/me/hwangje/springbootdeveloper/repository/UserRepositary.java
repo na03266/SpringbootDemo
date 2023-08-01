ec23d6711af9472c1da8f8874c48b5d762885b49
HwangjeNa <na03266@naver.com>
2023-08-02 08:21:09 +0900

    유저 리포지터리 생성

---
+++ src/main/java/me/hwangje/springbootdeveloper/repository/UserRepositary.java
@@ -0,0 +1,10 @@
+package me.hwangje.springbootdeveloper.repository;
+
+import me.hwangje.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepositary extends JpaRepository<User, Long> {
+    Optional<User> findBuEmail(String email); //이메일로 사용자 정보를 가져옴
+}
