51f1994d03f51af40275e808985361307cd6c516
HwangjeNa <na03266@naver.com>
2023-08-02 08:53:43 +0900

    실제 인증처리 시큐리티 설정 파일 생성 3 인증 관리자 관련 및 인코더 빈 등록

--- src/main/java/me/hwangje/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/me/hwangje/springbootdeveloper/config/WebSecurityConfig.java
@@ -3,8 +3,12 @@ package me.hwangje.springbootdeveloper.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.Security;
@@ -24,8 +28,8 @@ public class WebSecurityConfig {
     }
     
     // 특정 Http 요청에 대한 웹 기반 보안 구성
-@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests() //인증, 인가 설정
                 .requestMatchers("/login", "/signup", "/user").permitAll()
@@ -41,5 +45,23 @@ public class WebSecurityConfig {
                 .and()
                 .csrf().disable() //csrf 비활성화
                 .build();
-}
+    }
+
+    //인증 관리자 관련 설정
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+    BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailsService)
+            throws Exception {
+        return http.getSharedObject((AuthenticationManagerBuilder.class)
+                .userDetailsService(userService) //사용자 정보 서비스 설정
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    //패스워드 인코더로 사용할 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
