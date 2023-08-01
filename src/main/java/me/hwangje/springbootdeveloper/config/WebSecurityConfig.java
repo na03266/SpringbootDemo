07b2bbb920162030655d695e0fc3c6fe0904c292
HwangjeNa <na03266@naver.com>
2023-08-02 08:43:45 +0900

    실제 인증처리 시큐리티 설정 파일 생성 2 웹 기반 보안 구성

--- src/main/java/me/hwangje/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/me/hwangje/springbootdeveloper/config/WebSecurityConfig.java
@@ -3,7 +3,11 @@ package me.hwangje.springbootdeveloper.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Security;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
@@ -20,5 +24,22 @@ public class WebSecurityConfig {
     }
     
     // 특정 Http 요청에 대한 웹 기반 보안 구성
-
+@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests() //인증, 인가 설정
+                .requestMatchers("/login", "/signup", "/user").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin() //폼 기반 로그인 설정
+                .loginPage("/login")
+                .defaultSuccessUrl("/articles")
+                .and()
+                .logout()// 로그아웃 설정
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .and()
+                .csrf().disable() //csrf 비활성화
+                .build();
+}
 }
