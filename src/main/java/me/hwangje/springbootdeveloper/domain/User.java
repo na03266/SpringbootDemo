62d20e73aafe54a645e4660f4c83124b6859983a
HwangjeNa <na03266@naver.com>
2023-08-01 12:55:50 +0900

    의존성은 추가 완료 , but 소스 수정 진행 중 2

--- src/main/java/me/hwangje/springbootdeveloper/domain/User.java
+++ src/main/java/me/hwangje/springbootdeveloper/domain/User.java
@@ -5,6 +5,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
