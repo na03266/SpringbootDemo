ba45db5ee6ef2084b8726338466b730a4aa6ea70
HwangjeNa <na03266@naver.com>
2023-08-02 12:45:43 +0900

    패스워드를 인코딩용으로 등록한 빈을 이용해서 암호화 한 후 저장

---
+++ src/main/java/me/hwangje/springbootdeveloper/service/UserService.java
@@ -0,0 +1,26 @@
+package me.hwangje.springbootdeveloper.service;
+
+
+import lombok.RequiredArgsConstructor;
+import me.hwangje.springbootdeveloper.domain.User;
+import me.hwangje.springbootdeveloper.dto.AddUserRequest;
+import me.hwangje.springbootdeveloper.repository.UserRepositary;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepositary userRepositary;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepositary.save(User.builder()
+                .email(dto.getEmail())
+                //패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
+}
