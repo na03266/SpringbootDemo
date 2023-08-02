7f0efb474e99335d98dbfd29daaa10a198450239
NaHwangje <na03266@naver.com>
2023-08-03 08:40:25 +0900

    로그아웃 메서드 추가

--- src/main/java/me/hwangje/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/hwangje/springbootdeveloper/controller/UserApiController.java
@@ -1,9 +1,15 @@
 package me.hwangje.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.hwangje.springbootdeveloper.dto.AddUserRequest;
 import me.hwangje.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -16,5 +22,11 @@ public class UserApiController {
     public String signup(AddUserRequest request){
         userService.save(request); //회원가입 메서드 호출
         return "redirect:/login"; //회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
