4b1cf0842dbcaba3932884b8a39d45dfa0ee52d1
NaHwangje <na03266@naver.com>
2023-08-03 14:24:04 +0900

    어노테이션 수정

--- src/main/java/me/hwangje/springbootdeveloper/config/jwt/JwtProperties.java
+++ src/main/java/me/hwangje/springbootdeveloper/config/jwt/JwtProperties.java
@@ -2,13 +2,14 @@ package me.hwangje.springbootdeveloper.config.jwt;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
 @Component
-@Configuration("jwt") //자바 클레스에 프로피티 값을 가져와서 사용하는 애너테이션
+@ConfigurationProperties("jwt") //자바 클레스에 프로피티 값을 가져와서 사용하는 애너테이션
 public class JwtProperties {
     private String issuer;
     private String secretKey;
