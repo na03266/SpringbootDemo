a727a1a6debcf4b14ffede201fd4d4f2f84ee108
NaHwangje <na03266@naver.com>
2023-08-04 11:42:15 +0900

    토큰 활성 리포지터리 생성

---
+++ src/main/java/me/hwangje/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package me.hwangje.springbootdeveloper.repository;
+
+import me.hwangje.springbootdeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
