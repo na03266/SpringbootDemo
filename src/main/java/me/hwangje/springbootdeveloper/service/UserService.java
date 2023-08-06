d0e709ba8a9a5301d461a81ae2c4b15adec5d0f8
NaHwangje <na03266@naver.com>
2023-08-07 08:41:04 +0900

    생성자로 암호화인코더 코드 수정, 이메일 찾는 메서드 추가

--- src/main/java/me/hwangje/springbootdeveloper/service/UserService.java
+++ src/main/java/me/hwangje/springbootdeveloper/service/UserService.java
@@ -4,7 +4,6 @@ package me.hwangje.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.hwangje.springbootdeveloper.domain.User;
 import me.hwangje.springbootdeveloper.dto.AddUserRequest;
-import me.hwangje.springbootdeveloper.repository.UserRepositary;
 import me.hwangje.springbootdeveloper.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,17 +13,24 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Long save(AddUserRequest dto){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 //패스워드 암호화
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 .build()).getId();
     }
 
     public User findById(Long userId){
         return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->new IllegalArgumentException("Unexpected user"));
     }
 }
