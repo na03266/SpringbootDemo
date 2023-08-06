d7dc13b7a265f9c1c04495d15fb7165587052507
NaHwangje <na03266@naver.com>
2023-08-06 14:20:55 +0900

    사용자 정보를 조회해 유저 테이블에 사용자 정보가 있다면 리소스 서버에서 제공해주는 이름을 업데이트하고 없다면 user 테이블에 새 사용자를 생성해 데이터베이스에 저장하는 서비스를 구현 - OAuth 관련 키 저장 코드 추가

--- src/main/java/me/hwangje/springbootdeveloper/domain/User.java
+++ src/main/java/me/hwangje/springbootdeveloper/domain/User.java
@@ -29,10 +29,17 @@ public class User implements UserDetails { //UserDetails를 상속받아 인증 
     @Column(name = "password")
     private String password;
 
-    @Builder
-    public User(String email, String password, String auth){
+    //사용자 이름
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
+
+
+    @Builder //생성자에 nickname 추가
+    public User(String email, String password, String nickname){
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
     @Override //권한 변환
@@ -77,4 +84,13 @@ public class User implements UserDetails { //UserDetails를 상속받아 인증 
         // 계정이 사용 가능한지 확인하는 로직
         return true; // true -> 사용 가능
     }
+
+    // 사용자 이름 변경
+    public User update(String nickname){
+        this.nickname = nickname;
+
+        return this;
+    }
+
+
 }
