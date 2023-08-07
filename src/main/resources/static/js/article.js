c28aa789e543a7bc0bbc35c77f19d7b0fcacd5e7
NaHwangje <na03266@naver.com>
2023-08-07 12:31:33 +0900

    삭제기능 수정

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -4,13 +4,17 @@ const deleteButton = document.getElementById('delete-btn');
 if (deleteButton) {
     deleteButton.addEventListener('click', event => { //클릭 이벤트 발생시
         let id = document.getElementById('article-id').value;
-        fetch(`/api/articles/${id}`, { //패치 메서드로 딜리트 요청
-            method: 'DELETE'
-        })
-            .then(() => {
-                alert('삭제가 완료되었습니다.'); //요청 완료 메세지
-                location.replace('/articles'); //실행시 사용자의 화면을 시작 화면으로 이동.
-            });
+        function success() {
+            alert("삭제가 완료되었습니다.")
+            location.replace("/articles");
+        }
+
+        function fail(){
+            alert("삭제 실패했습니다.");
+            location.replace("/articles")
+        }
+
+        httpRequest("DELETE", "/api/articles/" +id, null, success, fail);
     });
 }
 
