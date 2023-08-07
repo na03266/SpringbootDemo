d13da69221962568a4af4ad78d65957adaad3049
NaHwangje <na03266@naver.com>
2023-08-07 09:18:05 +0900

    오타 수정

--- src/main/java/me/hwangje/springbootdeveloper/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ src/main/java/me/hwangje/springbootdeveloper/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -1,40 +1,40 @@
-package me.hwangje.springbootdeveloper.config.oauth;
-
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import me.hwangje.springbootdeveloper.util.CookieUtil;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequest;
 import org.springframework.web.util.WebUtils;
 
 public class OAuth2AuthorizationRequestBasedOnCookieRepository implements
-        AuthorizationRequestRepository<OAuth2AuthorizationRequest>{
+        AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
     public final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     private final static int COOKIE_EXPIRE_SECONDS = 18000;
 
     @Override
-    public OAuth2AuthorizationRequest remobeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response){
-        return this.loadAuthrizationRequest(request);
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        return this.loadAuthorizationRequest(request);
     }
 
     @Override
-    public OAuth2AuthorizationRequest loadAuthrizationRequest(HttpServletRequest request){
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
         Cookie cookie = WebUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
-        return CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.calss);
+        return CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class);
     }
 
     @Override
     public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request,
                                          HttpServletResponse response) {
-        if (authorizationRequest == null){
-            remobeAuthorizationRequest(request, response);
+        if (authorizationRequest == null) {
+            removeAuthorizationRequest(request, response);
             return;
         }
-        CookieUtil.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME, CookieUtil.serialize(authorizationRequest),
-                COOKIE_EXPIRE_SECONDS);
+        CookieUtil.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
+                CookieUtil.serialize(authorizationRequest), COOKIE_EXPIRE_SECONDS);
     }
 
-    public void remobeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response){
-        CookieUtil.deleteCookie(request,response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+    public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
+        CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
     }
 }
