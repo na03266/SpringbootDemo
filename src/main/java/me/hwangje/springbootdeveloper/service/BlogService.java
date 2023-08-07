97d1c628cc09ab84ed83a4015871cb47080d84c1
NaHwangje <na03266@naver.com>
2023-08-08 08:51:02 +0900

    글 수정 삭제시 글쓴이 확인 로직 추가

--- src/main/java/me/hwangje/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/hwangje/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,7 @@ import me.hwangje.springbootdeveloper.domain.Article;
 import me.hwangje.springbootdeveloper.dto.AddArticleRequest;
 import me.hwangje.springbootdeveloper.dto.UpdateArticleRequest;
 import me.hwangje.springbootdeveloper.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,16 +31,30 @@ public class BlogService {
     }
 
     public void delete(long id){
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
+
+
 
     @Transactional //트랜잭션 메서드
     public Article update(long id, UpdateArticleRequest request){
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " +id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+    private void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        if (!article.getAuthor().equals(userName)){
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
