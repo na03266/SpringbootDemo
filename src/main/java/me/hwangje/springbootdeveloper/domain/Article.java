06ff5b79841b293f463bba4542f861bf8d968b40
NaHwangje <na03266@naver.com>
2023-08-13 16:22:19 +0900

    application.yml.. 띄어쓰기 문제로 빌드 실패... 일부 오류 해결. 추가 오류 발생

--- src/main/java/me/hwangje/springbootdeveloper/domain/Article.java
+++ src/main/java/me/hwangje/springbootdeveloper/domain/Article.java
@@ -8,10 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
-import static lombok.AccessLevel.PROTECTED;
 
 @Entity //엔티티로 지정
 @Getter
