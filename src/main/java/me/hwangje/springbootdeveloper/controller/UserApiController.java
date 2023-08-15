a74dca9edace9818e8b2a3d2e32193c7c828fe62
NaHwangje <na03266@naver.com>
2023-08-15 09:39:53 +0900

    값 검증을 위한 의존성 추가

--- src/main/java/me/hwangje/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/hwangje/springbootdeveloper/controller/UserApiController.java
@@ -1,6 +1,5 @@
 package me.hwangje.springbootdeveloper.controller;
 
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
