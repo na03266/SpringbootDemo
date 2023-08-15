d91a594af634614e3554da9849bb29c3d63b4f2e
NaHwangje <na03266@naver.com>
2023-08-15 11:15:28 +0900

    메서드에 들어오는 파라미터 값 검증 애너테이션 추가

--- src/main/java/me/hwangje/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/hwangje/springbootdeveloper/controller/BlogApiController.java
@@ -8,6 +8,7 @@ import me.hwangje.springbootdeveloper.dto.UpdateArticleRequest;
 import me.hwangje.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -23,7 +24,8 @@ public class BlogApiController {
     //HTTP 메서드가 POST일 때 전달 받은 URL과 동일하면 메서드로 매핑
     @PostMapping("/api/articles")
     //@RequestBody로 요청 본문 값 매핑
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request, Principal principal){
+    public ResponseEntity<Article> addArticle(@RequestBody @Validated
+                                                  AddArticleRequest request, Principal principal){
         Article saveArticle = blogService.save(request, principal.getName());
         //요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
