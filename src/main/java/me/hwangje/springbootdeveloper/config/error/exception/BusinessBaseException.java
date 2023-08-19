102bdb1ebac0309766302e91386c6be4a0d36bf4
NaHwangje <na03266@naver.com>
2023-08-19 14:01:02 +0900

    create: 비즈니스 로직을 작성하다가 발생하는 예외를 모아두는 최상위 클래스

---
+++ src/main/java/me/hwangje/springbootdeveloper/config/error/exception/BusinessBaseException.java
@@ -0,0 +1,24 @@
+package me.hwangje.springbootdeveloper.config.error.exception;
+
+import me.hwangje.springbootdeveloper.config.error.ErrorCode;
+
+public class BusinessBaseException {
+
+    public class CustomBaseException extends RuntimeException{
+        private final ErrorCode errorCode;
+
+        public CustomBaseException(String message, ErrorCode errorCode) {
+            super(message);
+            this.errorCode = errorCode;
+        }
+
+        public CustomBaseException(ErrorCode errorCode) {
+            super(errorCode.getMessage());
+            this.errorCode = errorCode;
+        }
+
+        public ErrorCode getErrorCode() {
+            return errorCode;
+        }
+    }
+}
